c9e88ab9b78dd47cffc6ddbc3a98ffd39a67c8a2
Jordan Morris <jordanmorris@users.noreply.github.com>
2015-03-19 07:44:55 +1300

    forgot to version test class

---
+++ Mvc/Tests/DeadLockControllerTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+using Mvc.Controllers;
+using NUnit.Framework;
+
+namespace Mvc.Tests
+{
+    [TestFixture]
+    public class DeadLockControllerTests
+    {
+        [Test]
+        public void DeadLockControllerIndexActionRedirectsWithExpectedOutput()
+        {
+            //arrange
+            var controller = new DeadLockController();
+
+            //act
+            var actionResult = controller.Index() as RedirectToRouteResult;
+
+            //assert
+            // ReSharper disable once PossibleNullReferenceException
+            // justification: a null reference is a legit way for this test to fail
+            Assert.That(actionResult.RouteValues["Output"], Is.EqualTo("Success"));
+        }
+    }
+}
