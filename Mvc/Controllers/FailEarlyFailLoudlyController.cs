0d90d7a29a0724ae31f12955b772823023db41b7
Jordan Morris <jordanmorris@users.noreply.github.com>
2015-03-19 14:27:29 +1300

    added fail early example

---
+++ Mvc/Controllers/FailEarlyFailLoudlyController.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Mvc.Controllers
+{
+    public class FailEarlyFailLoudlyController : Controller
+    {
+        // GET: /Index/
+        public async Task<ActionResult> Index()
+        {
+            await DoSomethingAsync()
+                      .ConfigureAwait(continueOnCapturedContext: false);
+
+            var output = "Success. Http verb used: " +
+                         System.Web.HttpContext.Current.Request.HttpMethod;
+
+            return RedirectToAction("Index", "Home", new { Output = output });
+        }
+
+        private async Task DoSomethingAsync()
+        {
+            await Task.Delay(1000)
+                      .ConfigureAwait(continueOnCapturedContext: false);
+        }
+
+
+     }
+}
---
+++ Mvc/Controllers/SuccessfulContextAccess.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Mvc.Controllers
+{
+    public class SuccessfulContextAccessController : Controller
+    {
+        // GET: /Index/
+        public async Task<ActionResult> Index()
+        {
+            await DoSomethingAsync();
+
+            var output = "Success. Http verb used: " +
+                         System.Web.HttpContext.Current.Request.HttpMethod;
+
+            return RedirectToAction("Index", "Home", new { Output = output });
+        }
+
+        private async Task DoSomethingAsync()
+        {
+            await Task.Delay(1000)
+                      .ConfigureAwait(continueOnCapturedContext: false);
+        }
+
+
+     }
+}
