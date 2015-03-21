b22efebeb631fedbf42e4ed558ab36bea0434719
Jordan Morris <jordanmorris@users.noreply.github.com>
2015-03-21 20:50:42 +1300

    added parallel error handlng examples

---
+++ Mvc/Extras/ParallelErrorHandling.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
+
+namespace Mvc.Extras
+{
+    public class ParallelErrorHandling
+    {
+        static void Main(string[] args)
+        {
+            // Create some random data to process in parallel. 
+            // There is a good probability this data will cause some exceptions to be thrown. 
+            byte[] data = new byte[5000];
+            Random r = new Random();
+            r.NextBytes(data);
+
+            try
+            {
+                ProcessDataInParallel(data);
+            }
+
+            catch (AggregateException ae)
+            {
+                // This is where you can choose which exceptions to handle. 
+                foreach (var ex in ae.InnerExceptions)
+                {
+                    if (ex is ArgumentException)
+                        Console.WriteLine(ex.Message);
+                    else
+                        throw ex;
+                }
+            }
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+
+        private static void ProcessDataInParallel(byte[] data)
+        {
+            // Use ConcurrentQueue to enable safe enqueueing from multiple threads. 
+            var exceptions = new ConcurrentQueue<Exception>();
+
+            // Execute the complete loop and capture all exceptions.
+            Parallel.ForEach(data, d =>
+            {
+                try
+                {
+                    // Cause a few exceptions, but not too many. 
+                    if (d < 0x3)
+                        throw new ArgumentException(String.Format("value is {0:x}. Elements must be greater than 0x3.", d));
+                    else
+                        Console.Write(d + " ");
+                }
+                // Store the exception and continue with the loop.                     
+                catch (Exception e) { exceptions.Enqueue(e); }
+            });
+
+            // Throw the exceptions here after the loop completes. 
+            if (exceptions.Count > 0) throw new AggregateException(exceptions);
+        }
+    }
+}
---
+++ Mvc/Extras/PlinqErrorHandling.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace Mvc.Extras
+{
+    public class PlinqErrorHandling
+    {
+        // Paste into PLINQDataSample class. 
+        static void PLINQExceptions_1()
+        {
+            // Using the raw string array here. See PLINQ Data Sample. 
+            string[] customers = GetCustomersAsStrings().ToArray();
+
+
+            // First, we must simulate some currupt input.
+            customers[54] = "###";
+
+            var parallelQuery = from cust in customers.AsParallel()
+                                let fields = cust.Split(',')
+                                where fields[3].StartsWith("C") //throw indexoutofrange 
+                                select new { city = fields[3], thread = Thread.CurrentThread.ManagedThreadId };
+            try
+            {
+                // We use ForAll although it doesn't really improve performance 
+                // since all output is serialized through the Console.
+                parallelQuery.ForAll(e => Console.WriteLine("City: {0}, Thread:{1}", e.city, e.thread));
+            }
+
+            // In this design, we stop query processing when the exception occurs. 
+            catch (AggregateException e)
+            {
+                foreach (var ex in e.InnerExceptions)
+                {
+                    Console.WriteLine(ex.Message);
+                    if (ex is IndexOutOfRangeException)
+                        Console.WriteLine("The data source is corrupt. Query stopped.");
+                }
+            }
+        }
+
+
+
+
+
+        private static List<string> GetCustomersAsStrings()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
