fa9bdb984001814fa26d0cf5afff9c4057142183
Jordan Morris <jordanmorris@users.noreply.github.com>
2015-04-30 10:47:46 +1200

    improved commenting

--- Mvc/Controllers/SuccessfulContextAccess.cs
+++ Mvc/Controllers/SuccessfulContextAccess.cs
@@ -9,6 +9,9 @@ namespace Mvc.Controllers
         public async Task<ActionResult> Index()
         {
             await DoSomethingAsync();
+            // *** difference ***
+            // We allow the default behaviour, which is to resume on the 
+            // thread (captured context) we started with.
 
             var output = "Success. Http verb used: " +
                          System.Web.HttpContext.Current.Request.HttpMethod;
@@ -19,6 +22,8 @@ namespace Mvc.Controllers
         private async Task DoSomethingAsync()
         {
             await Task.Delay(1000)
+                //Note that it doesn't matter if async calls further down the stack
+                //let go of the context. ConfigureAwait has no upstream effect.
                       .ConfigureAwait(continueOnCapturedContext: false);
         }
 
