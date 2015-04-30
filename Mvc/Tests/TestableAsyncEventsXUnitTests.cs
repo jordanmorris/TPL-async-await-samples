fd5adc503e698692cdf0a5c90ee56171e79551fc
Jordan Morris <jordanmorris@users.noreply.github.com>
2015-04-30 13:54:08 +1200

    demonstration of xunit async test

---
+++ Mvc/Tests/TestableAsyncEventsXUnitTests.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using Mvc.Extras;
+using Xunit;
+
+namespace Mvc.Tests
+{
+    public class TestableAsyncEventsXUnitTests
+    {
+        [Fact]
+        public async void MyButtonActionAsyncDoesNotCrashWithXUnit()
+        {
+            //arrange
+            var buttonActionTask = new TestableAsyncEvents().MyButtonActionAsync();
+
+            //act
+            await buttonActionTask;
+
+            //assert
+            Assert.Equal(buttonActionTask.Status, TaskStatus.RanToCompletion);
+        }
+    }
+}
